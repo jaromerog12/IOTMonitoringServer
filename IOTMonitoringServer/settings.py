ec817bbb9da1f26404f3d1d26ffdaf56112c35fa
Jorge A. Romero Gutiérrez <jorgeandresromerog@gmail.com>
2024-09-11 20:38:10 -0500

    ajuste de ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.85.83.7"]
+ALLOWED_HOSTS = ["localhost", "3.89.40.239"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.227.47.124",  # Dirección IP de la base de datos
+        "HOST": "54.174.72.235",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.81.218.124"
+MQTT_HOST = "54.173.179.229"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
